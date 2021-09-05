33d97d93075eaffd0eb6cbd62b15825d5197de48
uwem <uwemneku@gmail.com>
2021-09-05 14:07:31 +0100

    set up basic navigation and FAB

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View style={{backgroundColor:'green', flex: 1, zIndex: 1,}}>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
